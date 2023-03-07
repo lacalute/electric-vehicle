b80e871d0a78fb7c86b36744b4520f15fd932444
yare-c <123@gmail.com>
2023-03-07 16:51:56 +0300

    add the map

--- el.py
+++ el.py
@@ -1,8 +1,10 @@
 # import necessary libraries
 import csv
 import matplotlib.pyplot as plt
-# import the web-server code
-from map import *
+from flask import Flask
+import folium
+# the initialization the app Flask
+app = Flask(__name__)
 
 # open the csv file
 with open('el.csv') as f:
@@ -12,7 +14,6 @@ with open('el.csv') as f:
     titles = x[0]
     # delete the titles
     x.pop(0)
-print(titles)
 
 # indexes of csv file
 # 3 - state
@@ -23,6 +24,7 @@ print(titles)
 # 14 - the vehicle location
 
 # the loop, which collect the list of states and list of cities for each state
+# for example --> {'WA': ['Seattle', 'Olympia', 'Tacoma', ...]}
 states = []
 cities = []
 for collect_states in x:
@@ -36,8 +38,8 @@ for collect_states in x:
 
 # the main func, which collect the necessary data for visualization (st - state, ct - city)
 def collect(st, ct):
+    global data_sorted
     global d
-    states = [] # the list with states
     cars = [] # the list with cars model 
     data = [] # the main list with dictionaries, filled with data about each model of car
     # the data dict has this structure:
@@ -51,10 +53,9 @@ def collect(st, ct):
     for coll_s in x:
         full_car = f'{coll_s[6]} {coll_s[7]} {coll_s[5]}' # the full name of car model
         if coll_s[3] == st and coll_s[2] == ct:
-            print(coll_s)
             # if model of car haven't been in list yet, then creating a new dict object
             if full_car not in cars:
-                data.append(dict({'model': full_car, 'points': coll_s[14], 'times': 0})) # fill the data list
+                data.append(dict({'model': full_car, 'times': 0})) # fill the data list
                 cars.append(full_car) # fill the list with cars models
             # but if model is repearing, then we change the var +1
             elif full_car in cars:
@@ -63,8 +64,6 @@ def collect(st, ct):
                         g['times'] = g['times'] + 1
     # after collect the data, we need to sort them and delete non-necessary data
     data_sorted = sorted(data, key=lambda item: item['times'])
-    for test in data_sorted:
-        print(test)
     for w in data_sorted:
         # all cars models, which are repeating 0 times - just deleting 
         if w['times'] == 0:
@@ -84,7 +83,38 @@ def collect(st, ct):
         plt.barh(labels, d)
         plt.show()
     plotting()
-# the main loop of program
+
+# the func, which work with coordinates
+# move the 'POINT ('-122.31307 47.66127)' into '[47.66127, -122.31307]'
+def coords(st, ct):
+    global models
+    data_map = []
+    for point in x:
+        if point[3] == st and point[2] == ct:
+            full_car = f'{point[6]} {point[7]} {point[5]}'
+            data_map.append(dict({'model': full_car, 'points': point[14]}))
+    for change in data_map:
+        # this is turning 'POINT ('-122.31307 47.66127)' into '[47.66127, -122.31307]' 
+        change['points'] = change['points'].replace("POINT (", "").replace(")", "").split(' ')
+        # the check for empty coordinates
+        try:
+            a = change['points'][1] 
+            b = change['points'][0] 
+            change['points'][0] = float(a)
+            change['points'][1] = float(b)
+        except IndexError:
+            data_map.remove(change)
+            print("There aren't any symbols")
+    # the final loop, which create the list with non-duplicated coordinates and with car models
+    cd = []
+    models = []
+    for times in data_map:
+        if times['points'] not in cd:
+            models.append(times['model'])
+            cd.append(times['points'])
+    return cd
+
+#the main loop of program
 status = True
 while status:
     try:
@@ -103,6 +133,7 @@ while status:
                 for test_city in cities:
                     if test_city['state'] == ask_state:
                         if ask_city in test_city['cities']:
+                            # call the func 
                             collect(ask_state, ask_city)
                             status = False
                         else:
@@ -110,12 +141,38 @@ while status:
             else:
                 print("Type another state")
         # create a map with markers
-        if choice == 2:
-            print("This option is developing right now. Sorry :)")
-            status = False
+        elif choice == 2:
+            print(states)
+            ask_state = str(input("Type the state -> "))
+            if ask_state in states:
+                for print_cities in cities:
+                    if print_cities['state'] == ask_state:
+                        print(print_cities['cities'])
+                        break
+                ask_city = str(input("Type the city -> "))
+                for test_city in cities:
+                    if test_city['state'] == ask_state:
+                        if ask_city in test_city['cities']:
+                            # starting the web server
+                            @app.route('/')
+                            def main():
+                                # the main class with the map
+                                m = folium.Map(location=coords(ask_state, ask_city)[0])
+                                for power in zip(coords(ask_state, ask_city), models):
+                                    try:
+                                        # create the markers with coordinates and the name of car model
+                                        folium.Marker(location=[power[0][0], power[0][1]], tooltip=power[1]).add_to(m)
+                                    except ValueError:
+                                        pass
+                                return m.get_root().render()
+                            if __name__ == '__main__':
+                                app.run()
+                            status = False
+                        else:
+                            print('Type another data') 
         else:
             print("Wrong option, please choose option again")
     except ValueError:
         print("Wrong data type, please type again")
 
-# Program by Lacalute !!!
+#Program by Lacalute !!!
--- map.py
+++
@@ -1,8 +0,0 @@
-# the test file
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def main():
-    return 'Test'
